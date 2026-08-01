44da4f0e7f353befec5265227cec7880955b078f
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-08-01 15:40:20 +0300

    Delete Natas/Scripts/Level 10->11 -2.py

--- Natas/Scripts/Level 10->11 -2.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-plaintext = b'{"showpassword":"yes","bgcolor":"#ffffff"}'
-key = b'kBSw'
-outtext = b''
-for i in range(len(plaintext)):
-    outtext += bytes([plaintext[i] ^ key[i % len(key)]])
-
-print(base64.b64encode(outtext).decode())
